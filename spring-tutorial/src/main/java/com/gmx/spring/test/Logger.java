08d0cc02ccb5af6d82fe1755e9825444f56acb6b
gintnerviktor <gintnerviktor@gmail.com>
2014-07-20 12:21:54 +0200

    byName most OK

--- spring-tutorial/src/main/java/com/gmx/spring/test/Logger.java
+++ spring-tutorial/src/main/java/com/gmx/spring/test/Logger.java
@@ -1,17 +1,31 @@
 package com.gmx.spring.test;
 
+/**
+ * @author viktor
+ *
+ */
 public class Logger {
 	private LogWriter consolwriter;
 	private LogWriter filewriter;
 	
-	public void setConsolwriter(LogWriter consolwriter) {
+	public void setConsolWriter(LogWriter consolwriter) {
 		this.consolwriter = consolwriter;
 	}
 
-	public void setFilewriter(LogWriter filewriter) {
+	/**
+	 * setFileWriter
+	 * A beanek-ben vigyázni kell az id ban található névre, mert a setFileWriter -> fileWriter lesz, azaz elmarad a set és a az első
+	 * betű kis méretűre vált !!!
+	 * @param filewriter
+	 */
+	public void setFileWriter(LogWriter filewriter) {
 		this.filewriter = filewriter;
 	}
 
+	/**
+	 * 
+	 * @param text
+	 */
 	public void writeConsol(String text) {
 		consolwriter.write(text);
 	}
