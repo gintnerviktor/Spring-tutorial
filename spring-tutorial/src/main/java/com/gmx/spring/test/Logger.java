06d8ccfe636f0f1d9e26c832c8eddf1ab8c7a324
gintnerviktor <gintnerviktor@gmail.com>
2014-07-23 07:40:15 +0200

    apróságok
    csak tesztelve volt

--- spring-tutorial/src/main/java/com/gmx/spring/test/Logger.java
+++ spring-tutorial/src/main/java/com/gmx/spring/test/Logger.java
@@ -1,13 +1,20 @@
 package com.gmx.spring.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Logger {
+	
+	@Autowired
 	private ConsolWriter consolwriter;
+	@Autowired
 	private FileWriter filewriter;
+	
 	
 	public void setConsolwriter(ConsolWriter consolwriter) {
 		this.consolwriter = consolwriter;
 	}
 
+	
 	public void setFilewriter(FileWriter filewriter) {
 		this.filewriter = filewriter;
 	}
